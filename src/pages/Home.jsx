b55155c4705b8b71947aaedd9eccd792f77e3530
flaviotech <flavio.nogueira.profissional@gmail.com>
2023-02-28 20:50:09 -0300

    Botão para a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import MovieCard from "../components/MovieCard";
+// import MovieCard from "../components/MovieCard";
+import { useNavigate } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 import "./MoviesGrid.css";
 
@@ -21,17 +23,46 @@ const Home = () => {
     getTopRatedMovies(topRatedUrl);
   }, []);
 
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
   console.log(topMovies);
 
   return (
     <div className="container">
       <h2 className="title">Melhores filmes:</h2>
-      <div className="movies-container">
-        {topMovies.length > 0 &&
-          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
     </div>
   );
+  // return (
+  //   <div className="container">
+  //     <h2 className="title">Melhores filmes:</h2>
+  //     <div className="movies-container">
+  //       {topMovies.length > 0 &&
+  //         topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Home;
