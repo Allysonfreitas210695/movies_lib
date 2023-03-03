c26ee6157bd4d303c3bc83c7dd600ce67a5dbe0b
natyvit <natalia16.nv@gmail.com>
2023-03-02 21:18:07 -0300

    ajustes de cores e gerais

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-
-
 const Footer = () => {
-    return (
-        <div className="Footer">
-            <h2 className="footer-h2"> 
-                &copy; 2023 MovieHunter - Todos os direitos reservados
+  return (
+    <div className="Footer">
+      <h2 className="footer-h2">
+        &copy; 2023 MoviesHunter - Todos os direitos reservados
+      </h2>
+    </div>
+  );
+};
 
-            </h2>
-            
-        </div>
-    );
-    }
-
- export default Footer;
+export default Footer;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -30,9 +30,9 @@ const Search = () => {
         Resultados para: <span className="query-text">{query}</span>
       </h2>
       <div className="movies-container">
-        {movies && movies.length > 0 &&
-          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
-        }
+        {movies &&
+          movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   );
