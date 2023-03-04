194e506232f9b06fa6ae5aa329442aeb10057059
natyvit <natalia16.nv@gmail.com>
2023-03-04 14:16:14 -0300

    ajustes responsivos

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -26,7 +26,7 @@ const Search = () => {
 
   return (
     <div className="container">
-      <h2 className="title">
+      <h2 className="title-busca">
         Resultados para: <span className="query-text">{query}</span>
       </h2>
       <div className="movies-container">
